1dd3305bfbb752cd131b8d50777b3c6ed842cd4f
CyLdS <cindyluanadasilva@gmail.com>
2024-08-15 09:59:01 -0300

    modulo peliculas

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PlanesController } from './planes/planes.controller';
 import { PlanesService } from './planes/planes.service';
+import { PeliculasController } from './peliculas/peliculas.controller';
+import { PeliculasService } from './peliculas/peliculas.service';
+import { UsuariosController } from './usuarios/usuarios.controller';
+import { UsuariosService } from './usuarios/usuarios.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, PlanesController],
-  providers: [AppService, PlanesService],
+  controllers: [AppController, PlanesController, PeliculasController, UsuariosController],
+  providers: [AppService, PlanesService, PeliculasService, UsuariosService],
 })
 export class AppModule {}
